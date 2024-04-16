6f0c054a96e3e46fcb06cb3580599127f06fb7c6
SIngleFon <timur5356@mail.ru>
2024-04-16 23:17:58 +0300

    Spring boot

---
+++ Seminar_2/src/main/java/ru/gb/my_first_crud/repository/UserRepository.java
@@ -0,0 +1,63 @@
+package ru.gb.my_first_crud.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.gb.my_first_crud.model.User;
+
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+    private final JdbcTemplate jdbc;
+
+    public UserRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public List<User> findAll() {
+        String sql = "SELECT * FROM userTable";
+
+        RowMapper<User> userRowMapper = (r, i) -> {
+            User rowObject = new User();
+            rowObject.setId(r.getInt("id"));
+            rowObject.setFirstName(r.getString("firstName"));
+            rowObject.setLastName(r.getString("lastName"));
+            return rowObject;
+        };
+
+        return jdbc.query(sql, userRowMapper);
+    }
+
+    public User save(User user) {
+        String sql = "INSERT INTO userTable (firstName,lastName) VALUES ( ?, ?)";
+        jdbc.update(sql, user.getFirstName(), user.getLastName());
+        return  user;
+    }
+
+    public void deleteById(int id){
+        String sql = "DELETE FROM userTable WHERE id=?";
+        jdbc.update(sql, id);
+    }
+//    public User update(User user){
+//        String sql = "UPDATE userTable SET (firstName, lastName) VALUES ( ?, ?) WHERE id = '?'";
+//        jdbc.update(sql, user.setFirstName("23"), user.setLastName("234"));
+//    }
+    public void update(User user) {
+        String sql = "UPDATE userTable SET firstName = ?, lastName = ? WHERE id = ?";
+        jdbc.update(sql, user.getFirstName(), user.getLastName(), user.getId());
+    }
+
+    public User findById(int id) {
+        String sql = "SELECT * FROM userTable WHERE id=?";
+        return jdbc.queryForObject(sql, new Object[]{id}, (rs, rowNum) -> {
+            User user = new User();
+            user.setId(rs.getInt("id"));
+            user.setFirstName(rs.getString("firstName"));
+            user.setLastName(rs.getString("lastName"));
+            return user;
+        });
+    }
+
+}
